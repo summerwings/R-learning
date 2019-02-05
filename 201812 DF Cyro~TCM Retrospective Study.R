14af7d8320e298495090a183500f18a01424f2c7
SUMMER ZHANG(张夏天) <summerwings@outlook.com>
2019-02-05 20:19:58 +0800

    Update 201812 DF Cyro~TCM Retrospective Study.R

--- 201812 DF Cyro~TCM Retrospective Study.R
+++ 201812 DF Cyro~TCM Retrospective Study.R
@@ -8,12 +8,15 @@
 
 #1.数据写入与合并
 #library(dplyr)
-#T1S<-read.csv('总1 单次 完整版一二三四漏网之鱼 323例.txt',sep = ';')
-#T2S<-read.csv('总2 单次 完整版一二三四漏网之鱼 323例.txt',sep = ';')
-#T1V<-read.csv('总1 多次 完整版二三四 197例.txt',sep = ';')
-#T2V<-read.csv('总2 多次 完整版二三四 200例.txt',sep = ';')
-#CQ<-read.csv('陈琪纳入病例名单.csv',sep = ',')
-#ZSQ<-read.csv('clipboard',sep = '')
+T1S<-read.csv('总1 单次 完整版一二三四漏网之鱼 323例.txt',sep = ';')
+T2S<-read.csv('总2 单次 完整版一二三四漏网之鱼 323例.txt',sep = ';')
+T1V<-read.csv('总1 多次 完整版二三四 197例.txt',sep = ';')
+T2V<-read.csv('总2 多次 完整版二三四 200例.txt',sep = ';')
+CQ<-read.csv('陈琪纳入病例名单.csv',sep = ',')
+ZSQ<-read.csv('ZSQ.csv',sep = ',')
+ZQ<-read.csv('ZQ.csv',sep = ',')
+HMK<-read.csv('HMK.csv',sep = ',')
+
 
 #1.1修改单次题头并合并
 names(T1S)[1]<-"ID"
@@ -112,31 +115,31 @@ POSTDATE.NUM<-c(grep(list("POSTOPDATE"), TT.TT),
 
 
 for (i in POSTDATE.NUM){
-      for (h in 1:length(TT$ID)){
-        
-        if(is.na(TT[h,i]-TT[h,2])){
-        }
-        else{
-        if(TT[h,i]-TT[h,2]<=30){
-            TT$post1month[h]<-TT[h,i+3]            
-        }
-        else if(TT[h,i]-TT[h,2]<=90){
-            TT$post3month[h]<-TT[h,i+3]  
-        }
-        else if(TT[h,i]-TT[h,2]<=180){
-          TT$post6month[h]<-TT[h,i+3]  
-        }        
-        else if(TT[h,i]-TT[h,2]<=365){
-          TT$post12month[h]<-TT[h,i+3]  
-        }     
-        else if(TT[h,i]-TT[h,2]<=1095){
-          TT$post36month[h]<-TT[h,i+3]  
-        }     
-        else if(TT[h,i]-TT[h,2]<=1825){
-          TT$post60month[h]<-TT[h,i+3]  
-        }     
+  for (h in 1:length(TT$ID)){
+    
+    if(is.na(TT[h,i]-TT[h,2])){
+    }
+    else{
+      if(TT[h,i]-TT[h,2]<=30){
+        TT$post1month[h]<-TT[h,i+3]            
       }
+      else if(TT[h,i]-TT[h,2]<=90){
+        TT$post3month[h]<-TT[h,i+3]  
       }
+      else if(TT[h,i]-TT[h,2]<=180){
+        TT$post6month[h]<-TT[h,i+3]  
+      }        
+      else if(TT[h,i]-TT[h,2]<=365){
+        TT$post12month[h]<-TT[h,i+3]  
+      }     
+      else if(TT[h,i]-TT[h,2]<=1095){
+        TT$post36month[h]<-TT[h,i+3]  
+      }     
+      else if(TT[h,i]-TT[h,2]<=1825){
+        TT$post60month[h]<-TT[h,i+3]  
+      }     
+    }
+  }
 }
 TT.TT<-c(colnames(TT)) 
 TT[grep("post1month",TT.TT):grep("post60month",TT.TT)]<-
@@ -209,11 +212,55 @@ TT.TT<-c(colnames(TT))
 colnames(TT)[grep(list("PREOPTOTAL"),TT.TT)]<-"术前病灶大小"
 colnames(TT)[grep(list("PREOPACTIV"),TT.TT)]<-"术前活性病灶大小"
 
-#术前分期
+#术前及诊断前分期
 TT.TT<-c(colnames(TT))
-colnames(TT)[grep(list("FDAFGSSCDI"),TT.TT)]<-"术前T分期"
-colnames(TT)[grep(list("FDAFGSSCD1"),TT.TT)]<-"术前N分期"
-colnames(TT)[grep(list("FDAFGSSCD2"),TT.TT)]<-"术前M分期"
+colnames(TT)[grep(list("FDAFGSSCDI"),TT.TT)]<-"Diagnosis.T.staging"
+colnames(TT)[grep(list("FDAFGSSCD1"),TT.TT)]<-"Diagnosis.N.staging"
+colnames(TT)[grep(list("FDAFGSSCD2"),TT.TT)]<-"Diagnosis.M.staging"
+
+
+TT$Diagnosis.T.staging[which(TT$Diagnosis.T.staging=="03")]<-"3"
+TT$Diagnosis.T.staging[which(TT$Diagnosis.T.staging=="04")]<-"4"
+
+
+TT$Diagnosis.N.staging[which(TT$Diagnosis.N.staging==4)]<-0
+TT$Diagnosis.N.staging[which(TT$Diagnosis.N.staging==5)]<-"x"
+
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==1)]<-"x"
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==2)]<-0
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==3)]<-"1a.PI"
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==4)]<-"1a.contr"
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==5)]<-"1b"
+TT$Diagnosis.M.staging[which(TT$Diagnosis.M.staging==6)]<-"1c"
+
+
+
+colnames(TT)[grep(list("BPSBLSSCDI"),TT.TT)]<-"Preoperative.T.staging"
+colnames(TT)[grep(list("BPSBLSSCD1"),TT.TT)]<-"Preoperative.N.staging"
+colnames(TT)[grep(list("BPSBLSSCD2"),TT.TT)]<-"Preoperative.M.staging"
+
+TT$Preoperative.T.staging[which(TT$Preoperative.T.staging=="03")]<-"3"
+TT$Preoperative.T.staging[which(TT$Preoperative.T.staging=="04")]<-"4"
+
+TT$Preoperative.N.staging[which(TT$Preoperative.N.staging==4)]<-0
+TT$Preoperative.N.staging[which(TT$Preoperative.N.staging==5)]<-"x"
+
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==1)]<-"x"
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==2)]<-0
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==3)]<-"1a.PI"
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==4)]<-"1a.contr"
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==5)]<-"1b"
+TT$Preoperative.M.staging[which(TT$Preoperative.M.staging==6)]<-"1c"
+
+
+
+
+
+#术前4期
+
+colnames(TT)[grep(list("PSBLSSCCL1"),TT.TT)]<-"Preoperative.4.staging"
+TT["Preoperative.4.staging"][TT["Preoperative.4.staging"]<=7]<-0
+TT["Preoperative.4.staging"][TT["Preoperative.4.staging"]>7]<-1
 
 #术前合并疾病
 TT.TT<-c(colnames(TT))
@@ -228,10 +275,10 @@ TT[HB.DIS][TT[HB.DIS]==1]=1
 TT[HB.DIS][TT[HB.DIS]==2]=1
 TT[HB.DIS][TT[HB.DIS]==3]=0
 colnames(TT)[HB.DIS]<-c("合并糖尿病","合并冠心病",
-                            "合并高血压","合并高脂血症",
-                            "合并脑卒中","合并重要脏器衰竭")
+                        "合并高血压","合并高脂血症",
+                        "合并脑卒中","合并重要脏器衰竭")
 
-#术后分期
+
 
 
 #术前体位+进针
@@ -241,17 +288,21 @@ colnames(TT)[grep(list("AMOUNT"),TT.TT)]<-"手术进针量"
 
 #基本信息
 TT.TT<-c(colnames(TT))
-    #性别 男1 女0
-colnames(TT)[grep(list("FEASEX"),TT.TT)]<-"性别"
-TT$性别<-TT$性别*-1+2
-TT<-data.frame(TT,"年龄"=NA)
-TT$年龄<-(TT$DATE-TT$FEABD)%/%10000
+#SEX 男1 女0
+colnames(TT)[grep(list("FEASEX"),TT.TT)]<-"SEX"
+TT$SEX<-TT$SEX*-1+2
+TT<-data.frame(TT,"AGE"=NA)
+TT$AGE<-(TT$DATE-TT$FEABD)%/%10000
 colnames(TT)[grep(list("FEASEX"),TT.TT)]
-    #吸烟 有1 无0
-colnames(TT)[grep(list("FEASMOKE"),TT.TT)]<-"吸烟史"
-TT["吸烟史"][TT["吸烟史"]==1]=0
-TT["吸烟史"][TT["吸烟史"]==2]=1
-TT["吸烟史"][TT["吸烟史"]==3]=NA
+#吸烟 有1 无0
+colnames(TT)[grep(list("FEASMOKE"),TT.TT)]<-"Smoking.History"
+TT["Smoking.History"][TT["Smoking.History"]==1]=0
+TT["Smoking.History"][TT["Smoking.History"]==2]=1
+TT["Smoking.History"][TT["Smoking.History"]==3]=NA
+
+colnames(TT)[grep(list("BZHEIGHT"),TT.TT)]<-"HEIGHT"
+colnames(TT)[grep(list("BZWEIGHT"),TT.TT)]<-"WEIGHT"
+
 
 #术后处理
 TT.TT<-c(colnames(TT))
@@ -282,9 +333,47 @@ colnames(TT)[grep(list("QCOTHERSYS"),TT.TT)]<-"全程其他全身疗法"
 colnames(TT)[grep(list("QCCHINESEM"),TT.TT)]<-"全程中医治疗"
 
 
+#中位生存日期
+TT<-data.frame(TT,"Middle.Survial.Time"=NA)
+TT["Middle.Survial.Time"]<-difftime(as.Date(as.character(TT$FOLLOWUPDA),format="%Y%m%d"),
+                             as.Date(as.character(TT$DATE),format="%Y%m%d"),units = "days")
 
-#
-QCVASCULAR
+#总生存日期
+TT<-data.frame(TT,"Total.Survial.Time"=NA)
+TT["Total.Survial.Time"]<-difftime(as.Date(as.character(TT$FOLLOWUPDA),format="%Y%m%d"),
+                             as.Date(as.character(TT$FDPATHLOGY),format="%Y%m%d"),units = "days")
+
+
+#CT值
+TT<-data.frame(TT,"Preoperative.Tumor.CT.Mean"=NA,"Preoperative.Tumor.CT.Var"=NA,
+               "Preoperative.ICEBALL.CT.Mean"=NA,"Preoperative.ICEBALL.CT.Var"=NA,
+               "Preoperative.Tumor.ECT.Mean"=NA,"Preoperative.Tumor.ECT.Var"=NA,
+               "Preoperative.ICEBALL.ECT.Mean"=NA,"Preoperative.ICEBALL.ECT.Var"=NA
+               )
+TT.TT<-c(colnames(TT))
+ct.num<-grep(list("TCPPS1"),TT.TT):grep(list("ICSIXTHP20"),TT.TT)
+
+for (i in TT[ct.num]){
+  i <- as.double(as.character(i))
+}
+
+  #开始列均值计算
+ct.num<-grep(list("TCPPS1"),TT.TT):grep(list("TCPPS5"),TT.TT)
+
+TT$TCPPS2<-as.double(as.character(TT$TCPPS2))
+TT$Preoperative.Tumor.CT.Mean<-rowMeans(TT[grep(list("TCPPS1"),TT.TT):grep(list("TCPPS5"),TT.TT)])
+
+
+
+TT$ICPPS2<-as.double(as.character(TT$ICPPS2))
+TT$ICPPS3<-as.double(as.character(TT$ICPPS3))
+TT$ICPPS4<-as.double(as.character(TT$ICPPS4))
+TT$Preoperative.ICEBALL.CT.Mean<-rowMeans(TT[grep(list("ICPPS1"),TT.TT):grep(list("ICPPS5"),TT.TT)])
+
+TT$Preoperative.Tumor.ECT.Mean<-rowMeans(TT[grep(list("TCPE1"),TT.TT):grep(list("TCPE5"),TT.TT)])
+
+TT$ICPE2<-as.double(as.character(TT$ICPE2))
+TT$Preoperative.ICEBALL.ECT.Mean<-rowMeans(TT[grep(list("ICPE1"),TT.TT):grep(list("ICPE5"),TT.TT)])
 
 
 
@@ -294,58 +383,191 @@ colnames(CQ)[5]<-"DATE"
 CQ.T<-merge(CQ,TT,by = c('ID',"DATE"))
 CQQ<-colnames(CQ.T)
 
-CQ.FD<-data.frame("ID"=CQ.T$ID,"DATE"=CQ.T$DATE,"NAME"=CQ.T$INFORMNAME
-                  ,"第一次随访死亡"=CQ.T$First.fellow.death,"第二次随访死亡"=CQ.T$Second.fellow.death
-                  ,"冰球覆盖率"=CQ.T$ICE.COVER.RATE,
+CQ.FD<-data.frame("ID"=CQ.T$ID,"DATE"=CQ.T$DATE,"NAME"=CQ.T$INFORMNAME,
+                  CQ.T["FOLLOWUPDA"],
+                  CQ.T[c("SEX","AGE","HEIGHT","WEIGHT","Smoking.History")],
+                  "第一次随访死亡"=CQ.T$First.fellow.death,
+                  "第二次随访死亡"=CQ.T$Second.fellow.death,             
+                  "冰球覆盖率"=CQ.T$ICE.COVER.RATE,
                   CQ.T[grep("左肺上叶",CQQ ):grep("贯穿左上下肺叶",CQQ )],
                   CQ.T[grep("左右两侧", CQQ):grep("包括皮肤",CQQ )],
-                  CQ.T[grep("post1month",CQQ ):grep("post60month",CQQ )])
+                  CQ.T[grep("post1month",CQQ ):grep("post60month",CQQ )],
+                  CQ.T["Middle.Survial.Time"],
+                  CQ.T["Total.Survial.Time"])
 
 CQQ<-colnames(CQ.FD)
 colnames(CQ.FD)[grep("post1month",CQQ ):grep("post60month",CQQ )]=c("1月疗效","3月疗效",
-         "6月疗效","12月疗效",
-         "36月疗效","60月疗效")
+                                                                    "6月疗效","12月疗效",
+                                                                    "36月疗效","60月疗效")
 
-write.csv(CQ.FD,'CQ.csv',row.names = FALSE)
+write.csv(CQ.FD,'20190205 CQ.csv',row.names = FALSE)
 
 #3合并张思奇数据
 colnames(ZSQ)[1]<-"ID"
 colnames(ZSQ)[4]<-"DATE"
 ZSQ.T<-merge(ZSQ,TT,by = c('ID',"DATE"))
 ZSQQ<-colnames(ZSQ.T)
-ZSQ.FD<-data.frame("ID"=ZSQ.T$ID,"DATE"=ZSQ.T$DATE,"NAME"=ZSQ.T$INFORMNAME,
-                   ZSQ.T[c("性别","年龄","吸烟史")],
-                   ZSQ.T[c("术前T分期","术前N分期","术前M分期")],
+ZSQ.FD<-data.frame("ID"=ZSQ.T$ID,"DATE"=ZSQ.T$DATE,
+                   ZSQ.T["FOLLOWUPDA"],
+                   ZSQ.T["Middle.Survial.Time"],
+                   "NAME"=ZSQ.T$INFORMNAME,
+                   ZSQ.T[c("SEX","AGE","HEIGHT","WEIGHT","Smoking.History")],
+                   ZSQ.T[c("Diagnosis.T.staging","Diagnosis.N.staging","Diagnosis.M.staging")],
                    ZSQ.T[c("合并肺病",
                            "合并糖尿病","合并冠心病",
                            "合并高血压","合并高脂血症",
                            "合并脑卒中","合并重要脏器衰竭")],
-                   ZSQ.T[c("全程手术"，"全程放射"，
-                   "全程化疗"，"全程血管介入"，
-                   "全程消融"，"全程其他全身疗法"，
-                   "全程中医治疗")]
+                   ZSQ.T[c("全程手术","全程放射",
+                           "全程化疗","全程血管介入",
+                           "全程消融","全程其他全身疗法",
+                           "全程中医治疗")],
+                   ZSQ.T[c("Preoperative.T.staging","Preoperative.N.staging","Preoperative.M.staging")],
                    "术前病灶大小"=ZSQ.T$术前病灶大小,
                    "术前活性病灶大小"=ZSQ.T$术前活性病灶大小,
                    "术中并发症"=ZSQ.T$COMPLICATI-1,
                    ZSQ.T[c("术中咳嗽","术中咯血","术中气胸",
-                         "术中发热","术中手术部位疼痛","术中肺部感染",
-                         "术中胸腔积液","术中冷休克","术中肿瘤溶解综合征",
-                         "术中灶周出血","术中神经损伤","术中心律失常")],
+                           "术中发热","术中手术部位疼痛","术中肺部感染",
+                           "术中胸腔积液","术中冷休克","术中肿瘤溶解综合征",
+                           "术中灶周出血","术中神经损伤","术中心律失常")],
                    ZSQ.T[c("术中咳嗽持续时间","术中咯血持续时间","术中气胸持续时间",
                            "术中发热持续时间","术中手术部位疼痛持续时间","术中肺部感染持续时间",
                            "术中胸腔积液持续时间","术中冷休克持续时间","术中肿瘤溶解综合征持续时间",
                            "术中灶周出血持续时间","术中神经损伤持续时间","术中心律失常持续时间")],
                    "术后并发症"=ZSQ.T$COMPLICAT1-1,
-                   ZSQ.T[c("术后咳嗽持续时间","术后咯血持续时间","术后气胸持续时间",
-                           "术后发热持续时间","术后手术部位疼痛持续时间","术后肺部感染持续时间",
-                           "术后胸腔积液持续时间","术后冷休克持续时间","术后肿瘤溶解综合征持续时间",
-                           "术后灶周出血持续时间","术后神经损伤持续时间","术后心律失常持续时间")],
+                   ZSQ.T[c("术后咳嗽","术后咯血","术后气胸",
+                           "术后发热","术后手术部位疼痛","术后肺部感染",
+                           "术后胸腔积液","术后冷休克","术后肿瘤溶解综合征",
+                           "术后灶周出血","术后神经损伤","术后心律失常")],
                    "手术体位"=ZSQ.T$手术体位,
                    "手术进针量"=ZSQ.T$手术进针量,
                    "冰球覆盖率"=ZSQ.T$ICE.COVER.RATE,
                    ZSQ.T[grep("左肺上叶",ZSQQ ):grep("贯穿左上下肺叶",ZSQQ )],
                    ZSQ.T[grep("左右两侧", ZSQQ):grep("包括皮肤",ZSQQ )],
                    ZSQ.T[c("术后抗生素","术后止血药物","术后碱化尿液药物")]
-                                      )
+)
 
-write.csv(ZSQ.FD,'ZSQ.csv',row.names = FALSE)
+
+
+
+write.csv(ZSQ.FD,'20190204 ZSQ.csv',row.names = FALSE)
+
+#3周琴数据合并
+colnames(ZQ)[1]<-"ID"
+colnames(ZQ)[5]<-"DATE"
+ZQ.T<-merge(ZQ,TT,by = c('ID',"DATE"))
+ZQQ<-colnames(ZQ.T)
+
+ZQ.FD<-data.frame("ID"=ZQ.T$ID,
+                  "DATE"=ZQ.T$DATE,
+                  ZQ.T["FOLLOWUPDA"],             
+                  ZQ.T["Middle.Survial.Time"],
+                  ZQ.T["Total.Survial.Time"],
+                  "第一次随访死亡"=ZQ.T$First.fellow.death,
+                  "第二次随访死亡"=ZQ.T$Second.fellow.death,   
+                  "NAME"=ZQ.T$INFORMNAME,
+                  ZQ.T[c("SEX","AGE","HEIGHT","WEIGHT","Smoking.History")],
+                  ZQ.T[c("Diagnosis.T.staging","Diagnosis.N.staging","Diagnosis.M.staging")],
+                  ZQ.T[c("合并肺病",
+                         "合并糖尿病","合并冠心病",
+                         "合并高血压","合并高脂血症",
+                         "合并脑卒中","合并重要脏器衰竭")],
+                  ZQ.T[c("全程手术","全程放射",
+                         "全程化疗","全程血管介入",
+                         "全程消融","全程其他全身疗法",
+                         "全程中医治疗")],
+                  ZQ.T[c("Preoperative.T.staging",
+                         "Preoperative.N.staging",
+                         "Preoperative.M.staging")],
+                  ZQ.T["Preoperative.4.staging"],
+                  "术前病灶大小"=ZQ.T$术前病灶大小,
+                  "术前活性病灶大小"=ZQ.T$术前活性病灶大小,
+                  "术中并发症"=ZQ.T$COMPLICATI-1,
+                  ZQ.T[c("术中咳嗽","术中咯血","术中气胸",
+                         "术中发热","术中手术部位疼痛","术中肺部感染",
+                         "术中胸腔积液","术中冷休克","术中肿瘤溶解综合征",
+                         "术中灶周出血","术中神经损伤","术中心律失常")],
+                  ZQ.T[c("术中咳嗽持续时间","术中咯血持续时间","术中气胸持续时间",
+                         "术中发热持续时间","术中手术部位疼痛持续时间","术中肺部感染持续时间",
+                         "术中胸腔积液持续时间","术中冷休克持续时间","术中肿瘤溶解综合征持续时间",
+                         "术中灶周出血持续时间","术中神经损伤持续时间","术中心律失常持续时间")],
+                  "术后并发症"=ZQ.T$COMPLICAT1-1,
+                  ZQ.T[c("术后咳嗽","术后咯血","术后气胸",
+                         "术后发热","术后手术部位疼痛","术后肺部感染",
+                         "术后胸腔积液","术后冷休克","术后肿瘤溶解综合征",
+                         "术后灶周出血","术后神经损伤","术后心律失常")],
+                  "手术体位"=ZQ.T$手术体位,
+                  "手术进针量"=ZQ.T$手术进针量,
+                  "冰球覆盖率"=ZQ.T$ICE.COVER.RATE,
+                  ZQ.T[grep("左肺上叶",ZQQ ):grep("贯穿左上下肺叶",ZQQ )],
+                  ZQ.T[grep("左右两侧", ZQQ):grep("包括皮肤",ZQQ )],
+                  ZQ.T[c("术后抗生素","术后止血药物","术后碱化尿液药物")]
+)
+write.csv(ZQ.FD,'20190204 ZQ.csv',row.names = FALSE)
+
+#4和孟珂数据合并
+colnames(HMK)[1]<-"ID"
+colnames(HMK)[4]<-"DATE"
+HMK.T<-merge(HMK,TT,by = c('ID',"DATE"))
+HMKQ<-colnames(HMK.T)
+
+HMK.FD<-data.frame("ID"=HMK.T$ID,
+                   "DATE"=HMK.T$DATE,
+                   HMK.T["FOLLOWUPDA"],
+                   HMK.T[c("Preoperative.Tumor.CT.Mean",
+                           "Preoperative.ICEBALL.CT.Mean",
+                           "Preoperative.Tumor.ECT.Mean",
+                           "Preoperative.ICEBALL.ECT.Mean")],
+                   HMK.T["Middle.Survial.Time"],
+                   HMK.T["Total.Survial.Time"],
+                   "第一次随访死亡"=HMK.T$First.fellow.death,
+                   "第二次随访死亡"=HMK.T$Second.fellow.death,   
+                   "NAME"=HMK.T$INFORMNAME,
+                   HMK.T[c("SEX","AGE","HEIGHT","WEIGHT","Smoking.History")],
+                   HMK.T[c("Diagnosis.T.staging","Diagnosis.N.staging","Diagnosis.M.staging")],
+                   HMK.T[c("合并肺病",
+                           "合并糖尿病","合并冠心病",
+                           "合并高血压","合并高脂血症",
+                           "合并脑卒中","合并重要脏器衰竭")],
+                   HMK.T[c("全程手术","全程放射",
+                           "全程化疗","全程血管介入",
+                           "全程消融","全程其他全身疗法",
+                           "全程中医治疗")],
+                   HMK.T[c("Preoperative.T.staging",
+                           "Preoperative.N.staging",
+                           "Preoperative.M.staging")],
+                   HMK.T["Preoperative.4.staging"],
+                   "术前病灶大小"=HMK.T$术前病灶大小,
+                   "术前活性病灶大小"=HMK.T$术前活性病灶大小,
+                   "术中并发症"=HMK.T$COMPLICATI-1,
+                   HMK.T[c("术中咳嗽","术中咯血","术中气胸",
+                           "术中发热","术中手术部位疼痛","术中肺部感染",
+                           "术中胸腔积液","术中冷休克","术中肿瘤溶解综合征",
+                           "术中灶周出血","术中神经损伤","术中心律失常")],
+                   HMK.T[c("术中咳嗽持续时间","术中咯血持续时间","术中气胸持续时间",
+                           "术中发热持续时间","术中手术部位疼痛持续时间","术中肺部感染持续时间",
+                           "术中胸腔积液持续时间","术中冷休克持续时间","术中肿瘤溶解综合征持续时间",
+                           "术中灶周出血持续时间","术中神经损伤持续时间","术中心律失常持续时间")],
+                   "术后并发症"=HMK.T$COMPLICAT1-1,
+                   HMK.T[c("术后咳嗽","术后咯血","术后气胸",
+                           "术后发热","术后手术部位疼痛","术后肺部感染",
+                           "术后胸腔积液","术后冷休克","术后肿瘤溶解综合征",
+                           "术后灶周出血","术后神经损伤","术后心律失常")],
+                   "手术体位"=HMK.T$手术体位,
+                   "手术进针量"=HMK.T$手术进针量,
+                   "冰球覆盖率"=HMK.T$ICE.COVER.RATE,
+                   HMK.T[grep("左肺上叶",HMKQ ):grep("贯穿左上下肺叶",HMKQ )],
+                   HMK.T[grep("左右两侧", HMKQ):grep("包括皮肤",HMKQ )],
+                   HMK.T[c("术后抗生素","术后止血药物","术后碱化尿液药物")],
+                   HMK.T[grep("post1month",HMKQ ):grep("post60month",HMKQ )]
+)
+HMKQ<-colnames(HMK.FD)
+colnames(HMK.FD)[grep("post1month",HMKQ ):grep("post60month",HMKQ )]=c("1月疗效","3月疗效",
+                                                                       "6月疗效","12月疗效",
+                                                                       "36月疗效","60月疗效")
+
+write.csv(HMK.FD,'20190204 HMK.csv',row.names = FALSE)
+
+
+
+
+
